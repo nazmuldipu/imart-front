122c9e74777be319609e491348b07848caa5c09b
Nazmul Alam <nazmuldipu@gmail.com>
2020-07-09 18:22:50 +0600

    shop model added

---
+++ src/dashboard/products/components/shop-form/shop-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-form',
+  templateUrl: './shop-form.component.html',
+  styleUrls: ['./shop-form.component.scss']
+})
+export class ShopFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
